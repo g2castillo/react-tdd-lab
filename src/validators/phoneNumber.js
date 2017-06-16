1d2ccdee7cc35c17e7b56984f039585782b4d61f
Adam Fabicki <adam@flashstock.com>
2017-06-15 22:50:42 -0400

    Solution 1: TDD simple phone number validator

--- src/validators/phoneNumber.js
+++ src/validators/phoneNumber.js
@@ -1,3 +1,3 @@
 export default ((value) => {
-    return undefined
+    return value.length === 0 || (value.match(/^[\d \-]+$/) && value.replace(/\D/g, '').length === 10) ? undefined : 'should be a valid phone number'
 })
--- src/validators/phoneNumber.test.js
+++ src/validators/phoneNumber.test.js
@@ -10,22 +10,42 @@ describe('PhoneNumberValidator', () => {
     })
 
     describe('647-555-1212', () => {
-        it('should return undefined')
+        it('should return undefined', () => {
+            const result = phoneNumberValidator('647-555-1212')
+
+            expect(result).toBeUndefined()
+        })
     })
 
     describe('647 555 1212', () => {
-        it('should return undefined')
+        it('should return undefined', () => {
+            const result = phoneNumberValidator('647 555 1212')
+
+            expect(result).toBeUndefined()
+        })
     })
 
     describe('words', () => {
-        it('should return an error')
+        it('should return an error', () => {
+            const result = phoneNumberValidator('647 ABC 1212')
+
+            expect(result).toEqual('should be a valid phone number')
+        })
     })
 
     describe('less than 10 digits', () => {
-        it('should return an error')
+        it('should return an error', () => {
+            const result = phoneNumberValidator('647 323')
+
+            expect(result).toEqual('should be a valid phone number')
+        })
     })
 
     describe('more than 10 digits', () => {
-        it('should return an error')
+        it('should return an error', () => {
+            const result = phoneNumberValidator('647 323 28378')
+
+            expect(result).toEqual('should be a valid phone number')
+        })
     })
 })
